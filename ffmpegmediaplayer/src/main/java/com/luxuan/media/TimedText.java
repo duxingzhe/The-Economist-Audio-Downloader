dab071e30660c5406fba8cfae48189ef5cc12f02
luxuan <myfather103@163.com>
2019-05-08 15:09:40 +0800

    实现FFmpegMediaPlayer。

--- ffmpegmediaplayer/src/main/java/com/luxuan/media/TimedText.java
+++ ffmpegmediaplayer/src/main/java/com/luxuan/media/TimedText.java
@@ -1,5 +1,31 @@
 package com.luxuan.media;
 
+/**
+ * Class to hold the timed text's metadata.
+ *
+ * {@hide}
+ */
 public class TimedText {
 
+    private static final int FIRST_PUBLIC_KEY=1;
+
+    public static final int KEY_DISPLAY_FLAGS=1;
+    public static final int KEY_STYLE_FLAGS=2;
+    public static final int KEY_BACKGROUND_COLOR_RGBA=3;
+    public static final int KEY_HIGHLIGHT_COLOR_RGBA=4;
+    public static final int KEY_SCROLL_DELAY=5;
+    public static final int KEY_WRAP_TEXT=6;
+    public static final int KEY_START_TIME=7;
+    public static final int KEY_STRUCT_BLINKING_TEXT_LIST=8;
+    public static final int KEY_STRUCT_FONT_LIST=9;
+    public static final int KEY_STRUCT_HIGHLIGHT_LIST=10;
+    public static final int KEY_STRUCT_HYPER_TEXT_LIST=11;
+    public static final int KEY_STRUCT_KARAOKE_LIST=12;
+    public static final int KEY_STRUCT_STYLE_LIST=13;
+    public static final int KEY_STRUCT_TEXT_POS=14;
+    public static final int KEY_STRUCT_JUSTIFICATION=15;
+    public static final int KEY_STRUCT_TEXT=16;
+
+    private static final int LST_PUBLIC_KEY=16;
+    private static final int FIRST_PRIVATE_KEY=101;
 }
