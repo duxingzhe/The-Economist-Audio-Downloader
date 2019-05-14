8433a045205d995e8ce4db4b3ccb42ea55bcd5e6
duxingzhe <myfathe103@163.com>
2019-05-14 18:59:14 +0800

    实现FFmpegMediaPlayer C部分。

--- ffmpegmediaplayer/src/main/jni/player/audioplayer.c
+++ ffmpegmediaplayer/src/main/jni/player/audioplayer.c
@@ -22,3 +22,29 @@ void bqPlayerCallback(SLAndroidSimpleBufferQueueItf bq, void *context)
     is->audio_callback(context, player->buffer, len);
     enqueue(&is->audio_player, (int16_t *)player->buffer, len);
 }
+
+void createEngine(AudioPlayer **ps)
+{
+    AudioPlayer *player=*ps;
+
+    player->buffer=NULL;
+    SLresult result;
+
+    result=slCreateEngine(&player->engineObject, 0, NULL, 0, NULL, NULL);
+    assert(SL_RESULT_SUCCESS==result);
+    (void)result;
+
+    result=(*player->engineObject)->Realize(player->engineObject, SL_BOOLEAN_FALSE);
+    assert(SL_RESULT_SUCCESS==result);
+    (void)result;
+
+    const SLInterfaceID ids[1]={SL_IID_ENVIRONMENTALREVERB};
+    const SLboolean req[1]={SL_BOOLEAN_FALSE};
+    result=(*player->engineObject)->CreateOutputMix(player->engineEngine, &player->outputMixObject, 0, ids, req);
+    assert(SL_RESULT_SUCCESS==result);
+    (void)result;
+
+    result=(*player->outputMixObject)->Realize(player->outputMixObject, SL_BOOLEAN_FALSE);
+    assert(SL_RESULT_SUCCESS==result);
+    (void)result;
+}
