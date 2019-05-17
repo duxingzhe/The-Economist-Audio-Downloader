b52bc7543c636d94836b13dcf6b2e887ed5b84a5
Lu Xuan <myfather103@163.com>
2019-05-17 08:32:15 +0800

    实现FFmpegMediaPlayer。

--- ffmpegmediaplayer/src/main/jni/player/videoplayer.h
+++ ffmpegmediaplayer/src/main/jni/player/videoplayer.h
@@ -22,9 +22,10 @@ void createVideoEngine(VideoPlayer **ps);
 void createScreen(VideoPlayer **ps, void *surface, int width, int height);
 void setSurface(VideoPlayer **ps, void *surface);
 struct SwsContext *createScaler(VideoPlayer **ps, AVCodecContext *codec);
-void *createBmp(VideoPlayer **ps, int width, int height)
+void *createBmp(VideoPlayer **ps, int width, int height);
 void *destroyBmp(VideoPlayer **ps, void *bmp);
 void updateBmp(VideoPlayer **ps, struct SwsContext *sws_ctx, AVCodecContext *pCodecCtx, void *bmp, AVFrame *pFrame, int width, int height);
 void displayBmp(VideoPlayer **ps, void *bmp, AVCodecContext *pCodecCtx, int width, int height);
 void shutdownVideoEngine(VideoPlayer **ps);
+
 #endif //NDK_VIDEOPLAYER_H
