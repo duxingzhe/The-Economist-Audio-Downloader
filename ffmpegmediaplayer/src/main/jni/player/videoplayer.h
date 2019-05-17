2cd83bcaa35275ad799493a7bc60b57938b72db6
Lu Xuan <myfather103@163.com>
2019-05-17 08:31:57 +0800

    实现FFmpegMediaPlayer。

--- ffmpegmediaplayer/src/main/jni/player/videoplayer.h
+++ ffmpegmediaplayer/src/main/jni/player/videoplayer.h
@@ -5,4 +5,26 @@
 #ifndef NDK_VIDEOPLAYER_H
 #define NDK_VIDEOPLAYER_H
 
+#include <stdint.h>
+#include <stdio.h>
+
+#include <libavcodec/avcodec.h>
+#include <libswscale/swscale.h>
+#include <ffmpeg_mediaplayer.h>
+#include <android/native_window_jni.h>
+
+typedef struct VideoPlayer
+{
+    ANativeWindow* nativeWindow;
+} VideoPlayer;
+
+void createVideoEngine(VideoPlayer **ps);
+void createScreen(VideoPlayer **ps, void *surface, int width, int height);
+void setSurface(VideoPlayer **ps, void *surface);
+struct SwsContext *createScaler(VideoPlayer **ps, AVCodecContext *codec);
+void *createBmp(VideoPlayer **ps, int width, int height)
+void *destroyBmp(VideoPlayer **ps, void *bmp);
+void updateBmp(VideoPlayer **ps, struct SwsContext *sws_ctx, AVCodecContext *pCodecCtx, void *bmp, AVFrame *pFrame, int width, int height);
+void displayBmp(VideoPlayer **ps, void *bmp, AVCodecContext *pCodecCtx, int width, int height);
+void shutdownVideoEngine(VideoPlayer **ps);
 #endif //NDK_VIDEOPLAYER_H
