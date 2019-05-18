a1611e0bb2eb926c4a1ca5223c386fd5359e453c
Lu Xuan <myfather103@163.com>
2019-05-18 23:21:19 +0800

    实现FFmpegMediaPlayer。

---
+++ ffmpegmediaplayer/src/main/jni/player/Mutex.h
@@ -0,0 +1,16 @@
+//
+// Created by Luxuan on 2019/5/18.
+//
+
+#ifndef NDK_MUTEX_H
+#define NDK_MUTEX_H
+
+#include <stdint.h>
+#include <sys/types.h>
+#include <time.h>
+
+#include <pthread.h>
+
+#include "Errors.h"
+
+#endif //NDK_MUTEX_H
--- ffmpegmediaplayer/src/main/jni/player/mediaplayer.h
+++ ffmpegmediaplayer/src/main/jni/player/mediaplayer.h
@@ -5,4 +5,29 @@
 #ifndef NDK_MEDIAPLAYER_H
 #define NDK_MEDIAPLAYER_H
 
+#include "Errors.h"
+#include <pthread.h>
+#include "Mutex.h"
+
+extern "C"
+{
+#include "ffmpeg_mediaplayer.h"
+}
+
+class MediaPlayerListener
+{
+public:
+    virtual void notify(int msg, int ext1, int ext2, int formatThread)=0;
+};
+
+class MediaPlayer
+{
+public:
+    MediaPlayer();
+    ~MediaPlayer();
+    
+    void disconnect();
+    status_t setDataSource(const char *url, const char *headers);
+    status_t setDataSource(int fd, int64_t offset, int64_t length);
+};
 #endif //NDK_MEDIAPLAYER_H
