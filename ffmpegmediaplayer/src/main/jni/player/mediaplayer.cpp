1b77ef25fbe85a33af39428eaa5fc0c7fe137202
Lu Xuan <myfather103@163.com>
2019-05-19 08:15:40 +0800

    实现FFmpegMediaPlayer。

--- ffmpegmediaplayer/src/main/jni/player/mediaplayer.cpp
+++ ffmpegmediaplayer/src/main/jni/player/mediaplayer.cpp
@@ -2,3 +2,25 @@
 // Created by Administrator on 2019/5/16.
 //
 
+#define LOG_TAG "FFmpegMediaPlayer"
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "Errors.h"
+#include <pthread.h>
+#include "mediaplayer.h"
+
+extern "C"
+{
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "ffmpeg_mediaplayer.h"
+}
+
+using namespace std;
+
+MediaPlayer::MediaPlayer()
+{
+
+}
+
