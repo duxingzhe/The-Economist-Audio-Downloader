b0923dfb24e2e87c739049b7a948fdfe7156768e
duxingzhe <myfathe103@163.com>
2019-05-31 18:51:24 +0800

    实现FFmpegMediaPlayer C部分。

--- ffmpegmediaplayer/src/main/jni/player/ffmpeg_utils.c
+++ ffmpegmediaplayer/src/main/jni/player/ffmpeg_utils.c
@@ -2,3 +2,114 @@
 // Created by Administrator on 2019/5/16.
 //
 
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <libavutil/opt.h>
+#include <ffmpeg_utils.h>
+
+#include <stdio.h>
+
+void set_shoucast_metadata(AVFormatContext *ic)
+{
+    char *value=NULL;
+
+    if(av_opt_get(ic, "icy_metadata_packet", 1, (uint8_t **)&value)<0)
+    {
+        value=NULL;
+    }
+
+    if(value &&value[0])
+    {
+        av_dict_set(&ic->metadata, ICY_METADATA, value, 0);
+    }
+}
+
+void set_duration(AVFormatContext *ic)
+{
+    char value[30]="0";
+    int duration=0;
+
+    if(ic)
+    {
+        if(ic->duration!=AV_NOPTS_VALUE)
+        {
+            duration=((ic->duration/AV_TIME_BASE)*1000);
+        }
+    }
+
+    sprintf(value, "%d", duration);
+    av_dict_set(&ic->metadata, DURATION, value, 0);
+}
+
+void set_codec(AVFormatContext *ic, int i)
+{
+    const char *codec_type=av_get_media_type_string(ic->streams[i]->codec->codec_type);
+
+    if(!codec_type)
+    {
+        return;
+    }
+
+    const char *codec_name=avcodec_get_name(ic->streams[i]->codec->codec_id);
+
+    if(strcmp(codec_type, "audio")==0)
+    {
+        av_dict_set(&ic->metadata, AUDIO_CODEC, codec_name, 0);
+    }
+    else if(codec_type && strcmp(codec_type, "video")==0)
+    {
+        av_dict_set(&ic->metadata, VIDEO_CODEC, codec_name, 0);
+    }
+}
+
+void set_rotation(AVFormatContext *ic, AVStream *audio_st, AVStream *video_st)
+{
+    if(!extract_metadata_internal(ic, audio_st, video_st, ROTATE)&&video_st&&video_st->metadata)
+    {
+        AVDictionaryEntry *entry=av_dict_get(video_st->metadata, ROTATE, NULL, AV_DICT_MATCH_CASE);
+
+        if(entry&&entry->value)
+        {
+            av_dict_set(&ic->metadata, ROTATE, entry->value, 0);
+        }
+        else
+        {
+            av_dict_set(&ic->metadata, ROTATE, "0", 0);
+        }
+    }
+}
+
+void set_framerate(AVFormatContext *ic, AVStream *audio_st, AVStream *video_st)
+{
+    char value[30]="0";
+
+    if(video_st&&video_st->avg_frame_rate.den && video_st->avg_frame_rate.num)
+    {
+        double d=av_q2d(video_st->avg_frame_rate);
+        uint64_t v=lrintf(d*100);
+        if(v%100)
+        {
+            sprintf(value, "%3.2f", d);
+        }
+        else if(v%(100*1000))
+        {
+            sprintf(value, "%1.0f", d);
+        }
+        else
+        {
+            sprintf(value, "%1.0fk", d/1000);
+        }
+
+        av_dict_set(&ic->metadata, FRAMERATE, value, 0);
+    }
+}
+
+void set_filesize(AVFormatContext *ic)
+{
+    char value[30]="0";
+
+    int64_t size=ic->pb?avio_size(ic->pb);
+    sprintf(value, "%"PRId64, size);
+    av_dict_set(&ic->metadata, FILESIZE, value, 0);
+}
