9a60556b7c016b2271ecf06e85c14ac8f9de8c40
Lu Xuan <myfather103@163.com>
2019-06-08 15:27:54 +0800

    实现FFmpegMediaMetadataRetriever Java部分。

--- ffmpegmediametaretriever/src/main/java/com/luxuan/ffmpegmediametaretriever/FFmpegMediaMetadataRetriever.java
+++ ffmpegmediametaretriever/src/main/java/com/luxuan/ffmpegmediametaretriever/FFmpegMediaMetadataRetriever.java
@@ -1,6 +1,16 @@
 package com.luxuan.ffmpegmediametaretriever;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
+import android.net.Uri;
+
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FFmpegMediaMetadataRetriever {
 
@@ -27,6 +37,104 @@ public class FFmpegMediaMetadataRetriever {
 
         native_init();
     }
+
+    private long mNativeContext;
+
+    public FFmpegMediaMetadataRetriever(){
+        native_setup();
+    }
+
+    public native void setDataSource(String path)throws IllegalStateException;
+
+    public void setDataSource(String uri, Map<String, String> headers) throws IllegalArgumentException{
+        int i=0;
+        String[] keys=new String[headers.size()];
+        String[] values=new String[headers.size()];
+        for(Map.Entry<String, String> entry: headers.entrySet()) {
+            keys[i]=entry.getKey();
+            values[i]=entry.getValue();
+            i++;
+        }
+
+        _setDataSource(uri, keys, values);
+    }
+
+    private native void _setDataSource(String uri, String[] keys, String[] values) throws IllegalArgumentException;
+
+    public native void setDataSource(FileDescriptor fd, long offset, long length) throws IllegalArgumentException;
+
+    public void setDataSource(FileDescriptor fd) throws IllegalArgumentException{
+        setDataSource(fd, 0, 0x7ffffffffffffffL);
+    }
+
+    public void setDataSource(Context context, Uri uri) throws IllegalArgumentException, SecurityException {
+        if(uri==null){
+            throw new IllegalArgumentException();
+        }
+
+        String scheme=uri.getScheme();
+        if(scheme==null||scheme.equals("file")){
+            setDataSource(uri.getPath());
+            return;
+        }
+
+        AssetFileDescriptor fd=null;
+        try{
+            ContentResolver resolver=context.getContentResolver();
+            try{
+                fd=resolver.openAssetFileDescriptor(uri, "r");
+            }catch(FileNotFoundException e) {
+                throw new IllegalArgumentException();
+            }
+
+            FileDescriptor descriptor=fd.getFileDescriptor();
+            if(!descriptor.valid()){
+                throw new IllegalArgumentException();
+            }
+
+            if(fd.getDeclaredLength()<0){
+                setDataSource(descriptor);
+            }else{
+                setDataSource(descriptor, fd.getStartOffset(), fd.getDeclaredLength());
+            }
+            return;
+        }catch(SecurityException ex){
+
+        }finally{
+            try{
+                if(fd!=null) {
+                    fd.close();
+                }
+            }catch(IOException ioException){
+
+            }
+        }
+
+        setDataSource(uri.toString());
+    }
+
+    public native String extractMetadata(String key);
+
+    public native String extractMetadataFromChapter(String key, int chapter);
+
+    public Metadata getMetadata(){
+        boolean update_only=false;
+        boolean apply_filter=false;
+
+        Metadata data=new Metadata();
+        HashMap<String, String> metadata=null;
+        if((metadata=native_getMetadata(update_only, apply_filter, metadata))==null){
+            return null;
+        }
+
+        if(!data.parse(metadata)){
+            return null;
+        }
+
+        return data;
+    }
+
+    private native final HashMap<String, String> native_getMetadata(boolean update_only, boolean apply_filter, HashMap<String, String> reply);
 
     private static native void native_init();
 
