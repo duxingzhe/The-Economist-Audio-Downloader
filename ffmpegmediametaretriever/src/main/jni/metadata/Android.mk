d59a1cbe37b6e6dfefaa29794804a1001b155dbd
luxuan <myfather103@163.com>
2019-08-08 11:10:40 +0800

    更改mk文件。

--- ffmpegmediametaretriever/src/main/jni/metadata/Android.mk
+++ ffmpegmediametaretriever/src/main/jni/metadata/Android.mk
@@ -2,6 +2,8 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_LIB_FFMPEG_FILE_PATH_PREFIX := $(LOCAL_PATH)/../../../../../commonlib/src/main/jni/ffmpeg
+
 LOCAL_MODULE := ffmpeg_mediametadataretriever_jni
 LOCAL_CFLAGS :=
 LOCAL_SRC_FILES := luxuan_media_MediaMetadataRetriever.cpp \
@@ -9,7 +11,7 @@ LOCAL_SRC_FILES := luxuan_media_MediaMetadataRetriever.cpp \
         ffmpeg_mediametadataretriever.c \
         ffmpeg_utils.c
 LOCAL_SHARED_LIBRARIES := libswresample-3 libswscale-5 libavcodec-58 libavformat-58 libavutil-56 libavfilter-7 libssl libcrypto
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../ffmpeg/include
+LOCAL_C_INCLUDES := $(LOCAL_LIB_FFMPEG_FILE_PATH_PREFIX)/include
 LOCAL_LDLIBS := -llog
 LOCAL_LDLIBS += -landroid
 LOCAL_LDLIBS += -ljnigraphics
--- ffmpegmediaplayer/src/main/jni/player/Android.mk
+++ ffmpegmediaplayer/src/main/jni/player/Android.mk
@@ -2,19 +2,20 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_LIB_FFMPEG_FILE_PATH_PREFIX := $(LOCAL_PATH)/../../../../../commonlib/src/main/jni/ffmpeg
+
 LOCAL_MODULE := ffmpeg_mediaplayer_jni
 SDL_PATH := ../SDL
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include $(LOCAL_PATH)/$(SDL_PATH)/src/video/android
 LOCAL_CFLAGS := 
-LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
-               luxuan_media_MediaPlayer.cpp \
+LOCAL_SRC_FILES := luxuan_media_MediaPlayer.cpp \
 		        mediaplayer.cpp \
 		        ffmpeg_mediaplayer.c \
                audioplayer.c \
                videoplayer.c \
                ffmpeg_utils.c
 LOCAL_SHARED_LIBRARIES := SDL2 libswresample-3 libswscale-5 libavcodec-58 libavformat-58 libavutil-56 libavfilter-7 libssl libcrypto
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../ffmpeg/include
+LOCAL_C_INCLUDES := $(LOCAL_LIB_FFMPEG_FILE_PATH_PREFIX)/include
 # for native audio
 LOCAL_LDLIBS += -lOpenSLES -lGLESv1_CM -lGLESv2
 # for logging
