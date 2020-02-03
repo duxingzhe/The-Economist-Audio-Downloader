eaee1f537b9795dc2a8e0431c2f75023806d3040
luxuan <myfather103@163.com>
2020-02-03 12:24:35 +0800

    修改编译脚本。

--- musicapp/src/main/java/com/luxuan/musicapp/MusicPlay.java
+++ musicapp/src/main/java/com/luxuan/musicapp/MusicPlay.java
@@ -2,14 +2,14 @@ package com.luxuan.musicapp;
 
 public class MusicPlay {
     static{
-        System.loadLibrary("avcodec-58");
-        System.loadLibrary("avdevice-58");
-        System.loadLibrary("avfilter-7");
-        System.loadLibrary("avformat-58");
-        System.loadLibrary("avutil-56");
-        System.loadLibrary("postproc-55");
-        System.loadLibrary("swresample-6");
-        System.loadLibrary("swscale-5");
+        System.loadLibrary("avcodec");
+        System.loadLibrary("avdevice");
+        System.loadLibrary("avfilter");
+        System.loadLibrary("avformat");
+        System.loadLibrary("avutil");
+        System.loadLibrary("postproc");
+        System.loadLibrary("swresample");
+        System.loadLibrary("swscale");
         System.loadLibrary("native-lib");
     }
 
